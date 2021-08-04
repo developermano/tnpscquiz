8e090a7f466b86ffe35e251ca2bd18e18de132c1
developermano <appdevelopermano@gmail.com>
2021-08-04 07:59:19 +0530

    added custom homepage

---
+++ lib/pages/firstpage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Firstpage extends StatefulWidget {
+  @override
+  _FirstpageState createState() => _FirstpageState();
+}
+
+class _FirstpageState extends State<Firstpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tnpscquiz/main.dart';
+import 'package:tnpscquiz/pages/export.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Firstpage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
