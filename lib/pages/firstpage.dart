e35d1947def5f2113a144bd1157dbe8c8dd4da8d
developermano <appdevelopermano@gmail.com>
2021-08-04 09:13:08 +0530

    added example carousalslider

--- lib/pages/firstpage.dart
+++ lib/pages/firstpage.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class Firstpage extends StatefulWidget {
@@ -20,9 +22,97 @@ class _FirstpageState extends State<Firstpage> {
               title: Text('tnpscquiz'),
               backgroundColor: Theme.of(context).primaryColor,
               centerTitle: true,
-            )
+            ),
+            SliverToBoxAdapter(
+              child: Carousalslider(),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Carousalslider extends StatelessWidget {
+  const Carousalslider({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: [
+        //1st Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/200/300"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //2nd Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/200/300"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //3rd Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/seed/picsum/200/300"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //4th Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/200"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+
+        //5th Image of Slider
+        Container(
+          margin: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            image: DecorationImage(
+              image: NetworkImage("https://picsum.photos/200"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+
+      //Slider Container properties
+      options: CarouselOptions(
+        height: 180.0,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enableInfiniteScroll: true,
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        viewportFraction: 0.8,
       ),
     );
   }
